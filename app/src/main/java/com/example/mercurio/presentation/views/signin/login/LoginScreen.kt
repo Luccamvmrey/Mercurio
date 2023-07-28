fcdfbc6d61c73e9640721e41c493c001712c7f77
Lucca Rey <104277321+Luccamvmrey@users.noreply.github.com>
2023-07-28 12:43:23 -0300

    Started: Front-end for user authentication.

---
+++ app/src/main/java/com/example/mercurio/presentation/views/signin/login/LoginScreen.kt
@@ -0,0 +1,11 @@
+package com.example.mercurio.presentation.views.signin.login
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun LoginScreen(
+    navController: NavController
+) {
+
+}
---
+++ app/src/main/java/com/example/mercurio/presentation/views/signin/login/LoginViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.mercurio.presentation.views.signin.login
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+
+) {
+}
