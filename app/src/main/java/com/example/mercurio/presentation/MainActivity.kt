47c1994aa9598e9610e5e5097aed507bebc98640
Lucca Rey <104277321+Luccamvmrey@users.noreply.github.com>
2023-07-28 12:42:07 -0300

    Added: Boilerplate code

--- app/src/main/java/com/example/mercurio/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mercurio/presentation/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.mercurio.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.mercurio.presentation.navigation.Navigation
 import com.example.mercurio.presentation.ui.theme.MercurioTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MercurioTheme {
-
+                Navigation()
             }
         }
     }
